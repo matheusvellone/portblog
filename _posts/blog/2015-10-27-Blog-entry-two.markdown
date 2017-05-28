170df1f0e2ae7a800300ef03075daf5897725d8b
Matheus Vellone <matheusvellone@hotmail.com>
2017-05-28 02:48:53 -0300

    Removed test posts. Renamed and refactored some files. Cleaned project

--- _posts/blog/2015-10-27-Blog-entry-two.markdown
+++
@@ -1,9 +0,0 @@
----
-layout: post
-title:  "Blog entry two"
-comments: true
-date:   2015-10-27 18:11:16
-categories: blog
----
-
-Test
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -9,16 +9,11 @@ $(function () {
 
 // smoothScroll function is applied from the document ready function
 function smoothScroll(duration) {
-    $('a[href^="#"]').on('click', function (event) {
-
-        var target = $($(this).attr('href'));
-
-        if (target.length) {
-            event.preventDefault();
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, duration);
-        }
+    $('a.back-to-top').on('click', function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0,
+        }, duration);
     });
 }
 
